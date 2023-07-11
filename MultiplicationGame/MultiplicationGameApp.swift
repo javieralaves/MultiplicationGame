8dc26eeea2f32e82a9530c5b9f22f5e32dcd9989
Javier Alaves <jayalaves@gmail.com>
2023-07-11 16:05:08 +0200

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Javier Alaves on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
